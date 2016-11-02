89e089fdbe5466c836a212dc4d3a7063fb55a555
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-11-02 14:32:39 +0000

    Removed min length of event name. Commented non-used css for the moment.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: { minimum: 5}
+  validates :title, presence: true
+  # for length specification use this syntax
+  # validates :title, presence: true, length: { minimum: 5}
 end
