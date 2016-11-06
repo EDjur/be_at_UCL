4897f5b9f99813a917e6c1c6e78435bcad4608b9
Max Hofer <maximilianhofer95@me.com>
2016-11-06 21:35:48 +0000

    implemented simple search interface

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
   # for length specification use this syntax
   validates :title, :text, presence: true, length: { minimum: 5, maximum: 2000}
+
+  def self.search(search)
+    where("title LIKE ?", "%#{search}%")
+  end
+
 end
