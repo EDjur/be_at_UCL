66de1215975c8303af0c642f5a044e54f1ff22e8
Max Hofer <maximilianhofer95@me.com>
2016-11-07 19:54:13 +0000

    expanded search to descriptions, miscellaneous adjustments for meeting with Library

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,10 @@
 class Event < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_one :category
+  # has_one :category
   # for length specification use this syntax
   validates :title, :text, presence: true, length: { minimum: 5, maximum: 2000}
 
   def self.search(search)
-    where("title LIKE ?", "%#{search}%")
-    where("category LIKE ?", "%#{search}%")
+    where("title LIKE ? OR category LIKE ? OR text LIKE ? ", "%#{search}%", "%#{search}%", "%#{search}%")
   end
-
 end
