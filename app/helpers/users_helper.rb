cdc22625a19fd2327860f92a9f3b42b4eec05cd3
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-11-12 18:47:19 +0000

    Added functionality to view a list of your attending events.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def attending_events
+
+    my_tickets = Ticket.where(["user_id = ?", current_user.id]).select("event_id").all
+    my_events = []
+    my_tickets.each do |ticket|
+      puts ticket.event_id
+      my_events << Event.where(["id = ?", ticket.event_id]).first
+    end
+
+    puts my_events
+    return my_events
+  end
 end
