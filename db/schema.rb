6ae1e6469ada4ac445fc1a5d72456f89d0cc6e4b
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-11-12 19:27:35 +0000

    Added host to events migration. Updated seeding.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112114455) do
+ActiveRecord::Schema.define(version: 20161112190754) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20161112114455) do
     t.time     "start_time"
     t.time     "end_time"
     t.string   "category"
+    t.integer  "host_user_id"
   end
 
   create_table "overall_averages", force: :cascade do |t|
