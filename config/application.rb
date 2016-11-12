332bc27065eac2e37e53140e0f2f13e084d71b7b
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-11-12 18:44:31 +0000

    Silenced deprecation warning for Rails 5.1

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module BeAtUcl
   class Application < Rails::Application
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.active_record.time_zone_aware_types = [:datetime, :time]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
