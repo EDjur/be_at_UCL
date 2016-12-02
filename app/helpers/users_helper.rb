c9a0addbbcd78ae46781806ae24a66144fbcbb39
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-12-02 15:08:20 +0000

    Added hosting events page on user profiel

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,4 +12,15 @@ module UsersHelper
   def has_ticket?(event)
     !Ticket.where(:event_id => event.id, :user_id => current_user.id).all.blank?
   end
+
+  def hosting_events
+    my_events = Event.where(:host_user_id => current_user.id)
+    my_hosting_events = []
+    my_events.each do |event|
+      puts event.title
+      my_hosting_events << event
+    end
+    puts my_events
+    return my_events
+  end
 end
